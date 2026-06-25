0707456643544ecb1ef99b3ece1c0df8ea08e9b1
Fitra1991 <maulidafitra91@gmail.com>
2026-06-26 00:10:53 +0800

    Update model.py

--- models/model.py
+++ models/model.py
@@ -2,36 +2,27 @@ import torch
 import torch.nn as nn
 from torchvision import models
 
-class MultimodalDenseNet(nn.Module):
 
-    def __init__(self,num_classes=4):
+class MultimodalDenseNet(nn.Module):
+    def __init__(self, num_classes=4):
         super().__init__()
 
-        self.rgb_net = models.densenet121()
-
+        # weights=None supaya Streamlit Cloud tidak mencoba download pretrained weights.
+        self.rgb_net = models.densenet121(weights=None)
         rgb_feat = self.rgb_net.classifier.in_features
         self.rgb_net.classifier = nn.Identity()
 
-        self.th_net = models.densenet121()
-
+        self.th_net = models.densenet121(weights=None)
         th_feat = self.th_net.classifier.in_features
         self.th_net.classifier = nn.Identity()
 
         self.classifier = nn.Sequential(
-             nn.Dropout(0.5),
-             nn.Linear(rgb_feat + th_feat, num_classes)
-
+            nn.Dropout(0.5),
+            nn.Linear(rgb_feat + th_feat, num_classes)
         )
 
-    def forward(self,rgb,th):
-
+    def forward(self, rgb, th):
         rgb_feat = self.rgb_net(rgb)
-
         th_feat = self.th_net(th)
-
-        fused = torch.cat(
-            [rgb_feat,th_feat],
-            dim=1
-        )
-
+        fused = torch.cat([rgb_feat, th_feat], dim=1)
         return self.classifier(fused)
